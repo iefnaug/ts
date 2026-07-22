f5531644a1c2030c04dfec33c8e2578d92fcb4ac
iefnaug <guanfei1993@gmail.com>
2026-07-22 14:52:32 +0800

    map

---
+++ src/basic/06map.ts
@@ -0,0 +1,19 @@
+{
+    let map = new Map<string, number>();
+    map.set("a", 1);
+    map.set("b", 2);
+    map.set("c", 3);
+    map.set("d", 4);
+    map.set("e", 5);
+    console.log(map)
+    console.log(map.get('a'))
+    console.log(map.has('a'))
+    console.log(map.size)
+    map.delete('a')
+    console.log(map)
+    console.log(map.keys());
+    console.log(map.values());
+    console.log(map.entries());
+    map.clear()
+    console.log(map.size);
+}
